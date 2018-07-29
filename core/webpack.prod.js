9115dbafa571ada6d9cf9b6ff5147ab9bc26b7d9
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2018-07-29 15:47:58 +0200

    fix webpack prod, remove problematic package

--- core/webpack.prod.js
+++ core/webpack.prod.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const baseWebpack = require('./webpack.base')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = merge(baseWebpack, {
   devtool: '#cheap-module-eval-source-map',
   plugins: [
@@ -14,19 +14,5 @@ module.exports = merge(baseWebpack, {
       'process.env.NODE_ENV': '"production"',
     }),
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function(module) {
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
-        )
-      },
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor'],
-    }),
   ],
 })
